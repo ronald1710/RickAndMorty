8864a1fc5a1c214aefa5bcfc6771efe152346298
Ronald Azofeifa <ronald.azofeifa@yahoo.com>
2023-03-13 18:46:49 -0600

    css

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -6,18 +6,20 @@ const Card = () => {
       <img className="imgcard" src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="" />
       </div>
       <div className="containerTitle">
-        <p>Adjudicator Rick</p>
+        <p className="title">Adjudicator Rick</p>
       </div>
-      <div>
-        <div>
+      <div className="containerInfoGeneral">
+      <div className="containerInfo">
+        <div className="containerInfo1">
           <p>RAZA</p>
           <p>ORIGEN</p>
           <p>APARICIONES</p>
         </div>
-        <div>
+        <div className="containerInfo2">
           <p>Humano</p>
           <p>Sin info</p>
           <p>1 Vez</p>
+        </div>
         </div>
       </div>
     </div>
